b35725b3543202e1201b13f71247ecb0f314601f
Donkor James <jamesdonkor987@gmail.com>
2024-04-17 21:38:18 +0000

    updated

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,5 @@
 -- This file prepares MySQL server
 
-mysql -u root -p
-
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF NOT EXISTS hbn_test@localhost IDENTI_FIED BY 'hbnb_test_pwd';
 GRANT ALL PREVILEGE ON hbnb_test_db.* TO hbnb_test@localhost;
