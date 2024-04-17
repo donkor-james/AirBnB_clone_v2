0a7599a3a21f57c0f3b9b290fb312b7ba95d1c52
Donkor James <jamesdonkor987@gmail.com>
2024-04-17 21:37:02 +0000

    updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 -- This file prepares MySQL server
 
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PREVILEGE ON hbnb_dev_db.* TO hbnb_dev@localhost;
-GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PREVILEGE ON `hbnb_dev_db`.* TO hbnb_dev@localhost;
+GRANT SELECT ON `performance_schema`.* TO hbnb_dev@localhost;
