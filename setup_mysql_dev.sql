4e8d80f71b19d6cef119efc38a59c3325a5ba15f
Donkor James <jamesdonkor987@gmail.com>
2024-04-18 00:47:15 +0000

    updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,20 @@
--- Creates MySQL server with database hbnb_dev_db
--- And previleges for new user
+-- Creates a MySQL server with:
+--   Database hbnb_dev_db.
+--   User hbnb_dev with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance.
 
--- Creates database if it doesn't exist
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
---create User hbnb_dev if not exist
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grants all privileges on hbnb_dev_db to hbnb_dev
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
---Grants SELECT privileges on performance schema to hbnb_dev
+-- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- Flush privileges to applied changes
-Flush PRIVILEGES;
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
