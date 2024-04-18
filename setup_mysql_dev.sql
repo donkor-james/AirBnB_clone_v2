0c7797c6b36bcd74814cc97d94286ef1db64f366
Donkor James <jamesdonkor987@gmail.com>
2024-04-18 00:32:48 +0000

    updated mysql files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,17 @@
--- This file prepares MySQL server
--- This file prepares MySQL server
+-- Creates MySQL server with database hbnb_dev_db
+-- And previleges for new user
 
+-- Creates database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@localhost IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PREVILEGE ON `hbnb_dev_db`.* TO hbnb_dev@localhost;
-GRANT SELECT ON `performance_schema`.* TO hbnb_dev@localhost;
+
+--create User hbnb_dev if not exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grants all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PREVILEGE ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+--Grants SELECT privileges on performance schema to hbnb_dev
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to applied changes
+Flush PREVILEGE;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- This file prepares MySQL server
 
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
-CREATE USER IF NOT EXISTS hbn_test@localhost IDENTI_FIED BY 'hbnb_test_pwd';
-GRANT ALL PREVILEGE ON hbnb_test_db.* TO hbnb_test@localhost;
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+CREATE USER IF NOT EXISTS 'hbn_test'@localhost IDENTI_FIED BY 'hbnb_test_pwd';
+GRANT ALL PREVILEGE ON `hbnb_test_db`.* TO `hbnb_test`@localhost;
+GRANT SELECT ON `performance_schema`.* TO `hbnb_test`@localhost;
