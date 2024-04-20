17286f8f5d748f5558e3dab8e3cda283114f261a
Donkor James <jamesdonkor987@gmail.com>
2024-04-20 20:16:09 +0000

    updated

--- models/city.py
+++ models/city.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-from models.state import State
-from sqlalchemy.orm import relationship
+#!/usr/bin/python
+""" holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
-    __tablename__ = "cities"
-
-    name = Column("name", String(128), nullable=False)
-    state_id = Column("state_id", String(60), ForeignKey(""))
-    places = relationship("place", backref="cities",
-                          cascade="all, delete-orphan")
-
-    name = ""
-    state_id = ""
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/user.py
+++ models/user.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
-from models.place import Place
-from sqlalchemy import String, Column
-from sqlalchemy.ext.declarative import declarative_base
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    __tablename__ = "users"
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user",
-                          cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user",
-                           cascade="all, delete-orphan")
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
