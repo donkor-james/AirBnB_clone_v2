1598fc845c5b8febac9f2fe6915d699c9fe6c883
James Donkor <jamesdonkor987@gmail.com>
2024-05-19 08:39:22 -0700

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,9 +19,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
 	""" return c followed by text in the <text> variable """
-	if "_" in text:
-		text = text.replace("_", " ")
-	return "C {}".format(text)
+	return "C {}".format(text.replace('_', ' '))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
