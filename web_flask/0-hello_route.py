35dd0c511fd9dc15ef3638af33b8b63882018084
Donkor James <jamesdonkor987@gmail.com>
2024-05-19 14:41:50 +0000

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def Home():
-    return "Hello HBNB"
+def index():
+    """ returns Hello HBNB!"""
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
