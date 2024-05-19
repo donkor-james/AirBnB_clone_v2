b6dea38a00337b5d109743a03c9e3fda1b74f3e2
James Donkor <jamesdonkor987@gmail.com>
2024-05-19 08:10:07 -0700

    upated

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script starts flask and renders routes from /c urls """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ returns HBNB """
+    return "HBNB"
+
+@app.route("/c/<text>")
+def c(text):
+	""" return c followed by text in the <text> variable """
+	if "_" in text:
+		text = text.replace("_", " ")
+	return "C {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
