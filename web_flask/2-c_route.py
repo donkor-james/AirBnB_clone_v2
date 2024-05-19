534cd7d7093e012abd7de63985791a3e4ee91c41
James Donkor <jamesdonkor987@gmail.com>
2024-05-19 09:32:24 -0700

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-start Flask application
+ starts Flask application and render /c routes
 """
 
 from flask import Flask
@@ -21,7 +21,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """display “C ” followed by the text variable"""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
