8a7d50b3a2c473a21561f91cf0b208b8acfef866
Donkor James <jamesdonkor987@gmail.com>
2024-05-19 14:51:59 +0000

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that start flask on port 5000 """
+""" starts flask application on port 5000 """
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,10 +10,12 @@ def index():
     """ returns Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-	""" returns HBNB """
-	return "HBNB"
+    """ returns HBNB """
+    return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
