f2e4fa17616d09e0a801df7751bf51602d87ee14
James Donkor <jamesdonkor987@gmail.com>
2024-05-19 09:28:03 -0700

    updated

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-""" a script starts flask  renders routes from /c urls """
-from flask import Flask
+"""
+start Flask application
+"""
 
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-    """ returns Hello HBNB!"""
-    return "Hello HBNB!"
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ returns HBNB """
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-	""" return c followed by text in the <text> variable """
-	return "C " + text.replace('_', ' ')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
+
