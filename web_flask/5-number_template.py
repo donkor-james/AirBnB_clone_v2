9faaf4ca0136f564d35add9167717337a4ff6661
Donkor James <jamesdonkor987@gmail.com>
2024-05-21 00:33:36 +0000

    updated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" start Flask and renders /number routes """
+""" start Flask application """
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -26,19 +26,19 @@ def cisfun(text):
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """ display python followed by the value of the text variable """
+    """ display “Python ”, followed by the value of the text variable """
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ display number followed by the value of the n variable """
-    return "{} is a number".format(n)
+def imanumber(n):
+    """ display “n is a number” only if n is an integer """
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number(n):
-    """ show a number passed by the variable n """
+def numbersandtemplates(n):
+    """ display a HTML page only if n is an integer """
     return render_template('5-number.html', n=n)
 
 
