bfa70d8b706bab29243897103e33a1ea06309940
Donkor James <jamesdonkor987@gmail.com>
2024-05-21 00:24:59 +0000

    updated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -30,12 +30,10 @@ def pythoniscool(text='is cool'):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number', strict_slashes=False)
 @app.route('/number/<n>', strict_slashes=False)
 def number(n):
     """ display number followed by the value of the n variable """
-    if isinstance(n, int):
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
