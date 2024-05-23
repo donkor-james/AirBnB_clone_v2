41e74fb178d2a32463303d99c73ded885f853a6f
Donkor James <jamesdonkor987@gmail.com>
2024-05-23 21:23:45 +0000

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def teardown(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ returns list of cities """
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
